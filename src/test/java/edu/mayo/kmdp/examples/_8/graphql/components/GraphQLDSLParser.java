428d36d19175323c513a2fda75c5534ca3331efc
Davide Sottara <Sottara.Davide@mayo.edu>
2021-02-22 18:02:38 -0600

    GraphQL (+ SPARQL) end-to-end example

---
+++ src/test/java/edu/mayo/kmdp/examples/_8/graphql/components/GraphQLDSLParser.java
@@ -0,0 +1,35 @@
+package edu.mayo.kmdp.examples._8.graphql.components;
+
+import static org.omg.spec.api4kp._20200801.AbstractCarrier.rep;
+import static org.omg.spec.api4kp._20200801.Answer.of;
+import static org.omg.spec.api4kp._20200801.taxonomy.krlanguage.KnowledgeRepresentationLanguageSeries.GraphQL_Schemas;
+import static org.omg.spec.api4kp._20200801.taxonomy.parsinglevel.ParsingLevelSeries.Abstract_Knowledge_Expression;
+
+import graphql.schema.idl.SchemaParser;
+import org.omg.spec.api4kp._20200801.AbstractCarrier;
+import org.omg.spec.api4kp._20200801.Answer;
+import org.omg.spec.api4kp._20200801.api.transrepresentation.v4.server.DeserializeApiInternal._applyLift;
+import org.omg.spec.api4kp._20200801.services.KnowledgeCarrier;
+import org.omg.spec.api4kp._20200801.taxonomy.parsinglevel.ParsingLevel;
+
+public class GraphQLDSLParser implements _applyLift {
+
+  @Override
+  public Answer<KnowledgeCarrier> applyLift(
+      KnowledgeCarrier carrier,
+      ParsingLevel parsingLevel,
+      String codedRep,
+      String config) {
+    if (Abstract_Knowledge_Expression.sameAs(parsingLevel)) {
+      SchemaParser schemaParser = new SchemaParser();
+      return of(carrier.asString()
+          .map(schemaParser::parse)
+          .map(ast -> AbstractCarrier.ofAst(ast)
+              .withAssetId(carrier.getAssetId())
+              .withArtifactId(carrier.getArtifactId())
+              .withRepresentation(rep(GraphQL_Schemas))));
+    } else {
+      return Answer.failed();
+    }
+  }
+}
